bfe8fff6e4df951970a1d3f3eb669e78c7d1981c
zurbzh <zurab.bzhalava@ki.se>
2018-10-17 14:23:42 +0200

    java-bash commands

--- src/main/java/org/ngseq/metagenomics/MultipleSingleNodeAssemler.java
+++ src/main/java/org/ngseq/metagenomics/MultipleSingleNodeAssemler.java
@@ -15,9 +15,7 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SQLContext;
 import org.seqdoop.hadoop_bam.FastqInputFormat;
-import org.seqdoop.hadoop_bam.FastqOutputFormat;
 import org.seqdoop.hadoop_bam.SequencedFragment;
-import scala.Tuple2;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -48,10 +46,7 @@ public class MultipleSingleNodeAssemler {
         Option ouOpt = new Option("out", true, "");
 
         options.addOption(new Option("localdir", true, "Absolute path to local temp dir ( YARN must have write permissions if YARN used)"));
-        options.addOption(new Option("merge", "Merge output"));
-        options.addOption(new Option("subdirs", "Read from subdirectories"));
         options.addOption(new Option("debug", "saves error log"));
-        options.addOption(new Option("bin", true, "Path to megahit binary, defaults calls 'megahit'"));
         options.addOption(new Option("single", "Single reads option, default is interleaved paired-end"));
         options.addOption(splitOpt);
         options.addOption(cOpt);
@@ -72,14 +67,8 @@ public class MultipleSingleNodeAssemler {
         String inputPath = (cmd.hasOption("in") == true) ? cmd.getOptionValue("in") : null;
         String outDir = (cmd.hasOption("out") == true) ? cmd.getOptionValue("out") : null;
         String localdir = cmd.getOptionValue("localdir");
-        boolean subdirs = cmd.hasOption("subdirs");
-        boolean debug = cmd.hasOption("debug");
         String readstype = (cmd.hasOption("single") == true) ? "-r" : "--12";
-        String bin = (cmd.hasOption("bin") == true) ? cmd.getOptionValue("bin") : "megahit";
 
-        int t = (cmd.hasOption("t") == true) ? Integer.valueOf(cmd.getOptionValue("t")) : 1;
-        double m = (cmd.hasOption("m") == true) ? Double.valueOf(cmd.getOptionValue("m")) : 0.9;
-        boolean mergeout = cmd.hasOption("merge");
 
         FileSystem fs = FileSystem.get(new Configuration());
         fs.mkdirs(fs, new Path(outDir), new FsPermission(FsAction.ALL, FsAction.ALL, FsAction.ALL));
@@ -107,84 +96,39 @@ public class MultipleSingleNodeAssemler {
 
         Dataset<Row> pairDF = pairEndKeys.join(df, pairEndKeys.col("key").equalTo(df.col("key"))).drop(pairEndKeys.col("key"));
 
-        Dataset<Row> forward = pairDF.filter(pairDF.col("read").equalTo(1)).sort("key");
-        Dataset<Row> reverse = pairDF.filter(pairDF.col("read").equalTo(2)).sort("key");
+        Dataset<Row> sortedPairDF = pairDF.sort("key");
 
 
-        String path = "hdfs:///Projects/indexes/Resources/ViraOutput/" + tempName + "/forward";
+        String path = "hdfs:///Projects/indexes/Resources/ViraOutput/" + tempName;
 
 
-        dfToFastq(forward).coalesce(100).saveAsNewAPIHadoopFile(path, Text.class, SequencedFragment.class, FastqOutputFormat.class, sc.hadoopConfiguration());
-        dfToFastq(reverse).coalesce(100).saveAsNewAPIHadoopFile("hdfs:///Projects/indexes/Resources/ViraOutput/" + tempName + "/reverse", Text.class, SequencedFragment.class, FastqOutputFormat.class, sc.hadoopConfiguration());
-
-
-        String pathtoforward = "hdfs:///Projects/indexes/Resources/ViraOutput/" + tempName + "/forward/part-*";
-        String pathtoreverse = "hdfs:///Projects/indexes/Resources/ViraOutput/" + tempName + "/reverse/part-*";
-
-        String mkDir = "mkdir " + localdir + "/" + tempName;
-
-        System.out.println("mkdir command  " + mkDir);
+        dfToFasta(sortedPairDF).coalesce(100).saveAsTextFile(path);
 
 
 
-        try {
-            ProcessBuilder pb_dir = new ProcessBuilder("/bin/sh", "-c", mkDir);
-            Process process = pb_dir.start();
-            BufferedReader err = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-            String e;
-            ArrayList<String> out = new ArrayList<String>();
-            while ((e = err.readLine()) != null) {
-                System.out.println(e);
-                out.add(e);
-            }
-            process.waitFor();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
 
 
-        String ass_cmd = "/srv/hops/hadoop/bin/hdfs dfs -text " + pathtoforward + " > "+localdir+"/"+tempName+"/forward.fq";
 
 
 
-        System.out.println("forward copy " + ass_cmd);
+
+        fs.copyToLocalFile(true, new Path(path), new Path(localdir));
+
+
+        String pathToLocalFasta = localdir + "/" + tempName;
+
+        String cat = "cat " + pathToLocalFasta + "/part-* > "+pathToLocalFasta+"/fasta.fa";
+        String rm = "rm "+pathToLocalFasta+"/part-*";
+        executeBashCommand(cat);
+        executeBashCommand(rm);
 
 
 
-        try {
-            ProcessBuilder pb = new ProcessBuilder("/bin/sh", "-c", ass_cmd);
-            Process process = pb.start();
-            BufferedReader err1 = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-            String e;
-            ArrayList<String> out = new ArrayList<String>();
-            while ((e = err1.readLine()) != null) {
-                System.out.println(e);
-                out.add(e);
-            }
-            process.waitFor();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        String ass_cmd1 = "/srv/hops/hadoop/bin/hdfs dfs -text " + pathtoreverse + " > "+localdir+"/"+tempName+"/reverse.fq";
-        System.out.println("reverse copy " + ass_cmd1);
 
 
-        try {
-            ProcessBuilder pb1 = new ProcessBuilder("/bin/sh", "-c", ass_cmd1);
-            Process process1 = pb1.start();
-            BufferedReader err2 = new BufferedReader(new InputStreamReader(process1.getErrorStream()));
-            String e;
-            ArrayList<String> out = new ArrayList<String>();
-            while ((e = err2.readLine()) != null) {
-                System.out.println(e);
-                out.add(e);
-            }
-            process1.waitFor();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
+
 
 
         String config_file = "echo \"max_rd_len=150\n" +
@@ -204,66 +148,61 @@ public class MultipleSingleNodeAssemler {
                 "#minimum aligned length to contigs for a reliable read location (at least 32 for short insert size)\n" +
                 "map_len=32\n" +
                 "#a pair of fastq file, read 1 file should always be followed by read 2 file\n" +
-                "q1="+localdir+"/"+tempName+"/forward.fq\n" +
-                "q2=" + localdir + "/" + tempName + "/reverse.fq\n \" >"+ localdir +"/soap.config.txt";
+                "p="+pathToLocalFasta+"/fasta.fa\n \" >"+pathToLocalFasta+"/soap.config.txt";
 
-        System.out.println("config file command " + config_file);
+        executeBashCommand(config_file);
 
-        try {
-            ProcessBuilder pb2 = new ProcessBuilder("/bin/sh", "-c", config_file);
-            Process process2 = pb2.start();
-            BufferedReader err3 = new BufferedReader(new InputStreamReader(process2.getErrorStream()));
-            String e;
-            ArrayList<String> out = new ArrayList<String>();
-            while ((e = err3.readLine()) != null) {
-                System.out.println(e);
-                out.add(e);
-            }
-            process2.waitFor();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        String Soapdenovo = "SOAPdenovo-63mer  all -s "+pathToLocalFasta+"/soap.config.txt -K 31 -R -o "+pathToLocalFasta+"/31 1 >"+pathToLocalFasta+"/ass.log 2 > "+pathToLocalFasta+"/ass.err";
 
 
-        String Soapdenovo = "SOAPdenovo-63mer  all -s "+localdir+"/soap.config.txt -K 31 -R -o "+localdir+"/31 1 >"+localdir+"/ass.log 2 > "+localdir+"/ass.err";
-        System.out.println("config file command " + Soapdenovo);
-
-
-
-        try {
-            ProcessBuilder pb3 = new ProcessBuilder("/bin/sh", "-c", Soapdenovo);
-            Process process3 = pb3.start();
-            BufferedReader err4 = new BufferedReader(new InputStreamReader(process3.getErrorStream()));
-            String e;
-            ArrayList<String> out = new ArrayList<String>();
-            while ((e = err4.readLine()) != null) {
-                System.out.println(e);
-                out.add(e);
-            }
-            process3.waitFor();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
         sc.stop();
 
     }
 
 
 
-    private static JavaPairRDD<Text, SequencedFragment> dfToFastq(Dataset<Row> df) {
 
-        return df.toJavaRDD().mapToPair(row -> {
 
-             String name = row.getAs("key");
+    private static JavaRDD<String> dfToFasta(Dataset<Row> df) {
 
+        return df.toJavaRDD().map(row -> {
+
+            String name = row.getAs("key");
+
+            if (row.getAs("read").equals(1)) {
+                name = name + "/1";
+            } else {
+                name = name + "/2";
+            }
+
+            String sequence = row.getAs("sequence").toString();
             //TODO: check values
-            Text t = new Text(name);
-            SequencedFragment sf = new SequencedFragment();
-            sf.setSequence(new Text(row.getAs("sequence").toString()));
-            sf.setQuality(new Text(row.getAs("quality").toString()));
+            String output = ">" + name + "\n" + sequence;
 
-            return new Tuple2<Text, SequencedFragment>(t, sf);
+            return output;
+
         });
+
+    }
+
+
+    private static void executeBashCommand(String command) {
+        try {
+
+            System.out.println("bash command  " + command);
+            ProcessBuilder pb = new ProcessBuilder("/bin/sh", "-c", command);
+            Process process = pb.start();
+            BufferedReader err = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+            String e;
+            ArrayList<String> out = new ArrayList<String>();
+            while ((e = err.readLine()) != null) {
+                System.out.println(e);
+                out.add(e);
+            }
+            process.waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
